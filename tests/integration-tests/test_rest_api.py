5eefc6efab893c94a1d4e11334b4dc473eb0a009
Jonathan Claudius <jclaudius@mozilla.com>
2018-09-19 14:02:49 -0400

    Add unit-test and integration-test suites

---
+++ tests/integration-tests/test_rest_api.py
@@ -0,0 +1,70 @@
+import json
+import os
+import sys
+import requests
+import pytest
+
+# export a API_URL environment varialble to be something like:
+# API_URL="https://something.execute-api.us-west-2.amazonaws.com/dev/"
+API_URL = os.environ.get("API_URL", None)
+
+
+def test_api_url_environtment_variable():
+    assert API_URL is not None
+
+
+if not API_URL.endswith("/"):
+    API_URL = API_URL + "/"
+
+
+def test_api_create():
+    r = requests.post('{}scans'.format(API_URL),
+                      data=json.dumps(
+        {"target": "ssh.mozilla.com", "port": 22}
+    )
+    )
+    entry = r.json()
+    assert isinstance(entry, dict)
+    assert entry.get('port') == 22
+    assert entry.get('target') == "ssh.mozilla.com"
+    assert isinstance(entry.get('id'), str)
+    assert isinstance(entry.get('createdAt'), int)
+    assert isinstance(entry.get('updatedAt'), int)
+
+
+def test_api_get():
+    r = requests.post('{}scans'.format(API_URL),
+                      data=json.dumps(
+        {"target": "ssh.mozilla.com", "port": 22}
+    )
+    )
+    entry = r.json()
+    assert isinstance(entry, dict)
+    assert isinstance(entry.get('id'), str)
+
+    r2 = requests.get('{}scans/{}'.format(API_URL, entry.get('id')))
+    entry2 = r2.json()
+
+    assert isinstance(entry2, dict)
+    assert isinstance(entry2.get('port'), int)
+    assert isinstance(entry2.get('target'), str)
+    assert isinstance(entry2.get('id'), str)
+    assert isinstance(entry2.get('createdAt'), int)
+    assert isinstance(entry2.get('updatedAt'), int)
+    assert entry2.get('id') == entry.get('id')
+    assert entry2.get('port') == entry.get('port')
+    assert entry2.get('target') == entry.get('target')
+    assert entry2.get('createdAt') == entry.get('createdAt')
+    assert entry2.get('updatedAt') == entry.get('updatedAt')
+
+
+def test_api_list():
+    r = requests.get('{}scans'.format(API_URL))
+    assert isinstance(r.json(), list)
+    for entry in r.json():
+        assert isinstance(entry, dict)
+        assert isinstance(entry.get('port'), int)
+        assert isinstance(entry.get('target'), str)
+        assert isinstance(entry.get('id'), str)
+        assert isinstance(entry.get('createdAt'), int)
+        assert isinstance(entry.get('updatedAt'), int)
--- tests/unit-tests/test_target.py
+++ tests/unit-tests/test_target.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.append(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), '../'))
+    os.path.dirname(os.path.abspath(__file__)), '../..'))
 
 from scans import Target
 
