72c5d960b3c43bbe7493ae343347f9d1da82b24d
Jonathan Claudius <jclaudius@mozilla.com>
2018-09-26 10:31:29 -0400

    Clean up response security headers logic

--- tests/unit-tests/test_response.py
+++ tests/unit-tests/test_response.py
@@ -28,21 +28,34 @@ class TestResponse():
             "body": json.dumps({'foo': 'bar'})
         }
 
+        new_headers_expectation = Response.SECURITY_HEADERS
+
         response = Response(original_response)
 
         assert type(Response(original_response)) == Response
         assert response.with_security_headers() == {'body': '{"foo": "bar"}',
-                                                    'headers': {'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, HEAD',
-                                                                'Access-Control-Allow-Origin': '*',
-                                                                'Access-Control-Max-Age': '86400',
-                                                                'Cache-control': 'no-store',
-                                                                'Content-Security-Policy': "default-src 'none'; frame-ancestors 'none'; script-src 'none'; upgrade-insecure-requests",
-                                                                'Pragma': 'no-cache',
-                                                                'Referrer-Policy': 'no-referrer',
-                                                                'Strict-Transport-Security': 'max-age = 15768000; includeSubDomains',
-                                                                'X-Content-Type-Options': 'nosniff',
-                                                                'X-Download-Options': 'noopen',
-                                                                'X-Frame-Options': 'DENY',
-                                                                'X-Permitted-Cross-Domain-Policies': 'none',
-                                                                'X-XSS-Protection': '1; mode = block'},
+                                                    'headers': new_headers_expectation,
+                                                    'statusCode': 200}
+
+    def test_with_security_headers_and_preexisting(self):
+        original_headers = {
+            'foo': 'bar'
+        }
+
+        original_response = {
+            "statusCode": 200,
+            "body": json.dumps({'foo': 'bar'}),
+            # We're adding to make sure it's not overwritten
+            'headers': original_headers
+        }
+
+        new_headers_expectation = {}
+        new_headers_expectation.update(original_headers)
+        new_headers_expectation.update(Response.SECURITY_HEADERS)
+
+        response = Response(original_response)
+
+        assert type(Response(original_response)) == Response
+        assert response.with_security_headers() == {'body': '{"foo": "bar"}',
+                                                    'headers': new_headers_expectation,
                                                     'statusCode': 200}
